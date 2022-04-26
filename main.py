725c2b2196ea9a5fc9104bbb2af8c08aa6ed4b9b
Alex Rusnak <atrusnak@wisc.edu>
2022-04-26 00:26:36 -0500

    fixed preview

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from time import sleep
 from picamera import PiCamera
 
 camera = PiCamera()
-camera.resolution = (2560,1936)
+camera.resolution = (1500,1500)
 camera.start_preview()
 sleep(5)
 camera.capture('test.jpg')
