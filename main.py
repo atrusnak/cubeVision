a13b16f2f32723fd72105ed51343b592f489f10e
Alex Rusnak <atrusnak@wisc.edu>
2022-04-26 00:11:16 -0500

    try to take an image

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-hi
+import cv2 as cv
 
+cam_port=0
+cam = cv.VideoCapture(cam_port)
+
+result, image = cam.read()
+
+
+if result: 
+    imshow("GeeksForGeeks", image)
+
+    imwrite("hello.png", image)
+
+    waitKey(0)
+    destroyWindow("GeeksForGeeks")
+
+
+else:
+    print("did'nt work")
