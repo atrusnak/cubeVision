c96df151c1ea3c0bb875b54a802aeb7bc396cdc6
Alex <atrusnak@wisc.edu>
2022-07-22 01:17:22 -0500

    overall cube bounding box now working

--- convertHDF5toJPG.py
+++ convertHDF5toJPG.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
 from PIL import Image
 
-for i in range(3):
+for i in range(100):
     hdf = h5py.File('outputTest/'+str(i)+".hdf5",'r')
     array = hdf["colors"][:]
     img = Image.fromarray(array.astype('uint8'), 'RGB')
